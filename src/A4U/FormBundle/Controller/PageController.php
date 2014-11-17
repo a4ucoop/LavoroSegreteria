22c9383e1f56062a24bd01670cecf3f2331ccdea
Michelangelo Diamanti <michelangelo.diamanti@gmail.com>
2014-11-17 22:53:07 +0100

    Creato classi per form, inserito validazione lato client & server

--- src/A4U/FormBundle/Controller/PageController.php
+++ src/A4U/FormBundle/Controller/PageController.php
@@ -6,6 +6,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use A4U\FormBundle\Entity\PorteAperteEstate;
 use Symfony\Component\HttpFoundation\Request;
+use A4U\FormBundle\Form\Type\PorteAperteEstateType;
 
 class PageController extends Controller
 {
@@ -16,60 +17,17 @@ class PageController extends Controller
 
 	public function porte_aperte_estateAction(Request $request)
 	{
-        // crea un task fornendo alcuni dati fittizi per questo esempio
+
         $PorteAperteEstate = new PorteAperteEstate();
-        //$task->setName('Gesù');
-        //$task->setSurname('cristo');
 
-        $form = $this->createFormBuilder($PorteAperteEstate)
-            ->add('name', 'text')//, array('required' => false))
-            ->add('surname', 'text')
-            ->add('address', 'text')
-            ->add('cap', 'text')
-            ->add('city', 'text')
-            ->add('email', 'text')
-            ->add('phone', 'text')
-            ->add('birthDate', 'birthday')
-            ->add('birthPlace', 'text')
-            ->add('attendedSchool', 'text')
-            ->add('attendedSchoolCity', 'text')
-            ->add('hasAttendedtoOtherActivities', 'choice', array(
-                'choices'   => array('T' => 'Si', 'F' => 'No'),
-                'required'  => false,
-                ))
-            ->add('otherActivity', 'text')
-            ->add('reference', 'text')
-            ->add('otherReference', 'text')
-            ->add('unicamCourse', 'text')
-            ->add('submissionDate', 'date')
-
-            ->add('save', 'submit', array('label' => 'Iscriviti'))
-            ->getForm();
+        $form = $this->createForm('porteAperteEstate', $PorteAperteEstate);
 
         $form->handleRequest($request);
 
-        //Form inviato
+        //Form inviato, viene validato
         if ($form->isValid()) {
             // esegue alcune azioni, come ad esempio salvare il task nella base dati
-        
-        // I validatori lato server sono ancora da aggiustare per ora i campi sono validati in HTML5 (a quanto pare)
 
-        /*    $validator = $this->get('validator');
-            $errori = $validator->validate($form);
-            
-            if (count($errori) > 0) {
-                /*
-                 * Usa un metodo a __toString sulla variabile $errors, che è un oggetto
-                 * ConstraintViolationList. Questo fornisce una stringa adatta
-                 * al debug
-                 */
-         /*       $errorsString = (string) $errori;
-
-                return new Response($errorsString);
-            }
-
-            return new Response('Il nome è valido! Sì!');
-        */
             return $this->redirect($this->generateUrl('a4_u_form_success'));
         }
 
---
+++ src/A4U/FormBundle/Form/Type/PorteAperteEstateType.php
@@ -0,0 +1,44 @@
+<?php
+
+// src/A4U/FormBundle/Form/Type/PorteAperteType.php
+
+namespace A4U\FormBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class PorteAperteEstateType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', 'text')//, array('required' => false))
+            ->add('surname', 'text')
+            ->add('address', 'text')
+            ->add('cap', 'text')
+            ->add('city', 'text')
+            ->add('email', 'text')
+            ->add('phone', 'text')
+            ->add('birthDate', 'birthday')
+            ->add('birthPlace', 'text')
+            ->add('attendedSchool', 'text')
+            ->add('attendedSchoolCity', 'text')
+            ->add('hasAttendedtoOtherActivities', 'choice', array(
+                'choices'   => array('T' => 'Si', 'F' => 'No')
+                ))
+            ->add('activity', 'text')
+            ->add('otherActivity', 'text')
+            ->add('reference', 'text')
+            ->add('otherReference', 'text')
+            ->add('unicamCourse', 'text')
+            ->add('submissionDate', 'date')
+
+            ->add('save', 'submit', array('label' => 'Iscriviti'));
+    }
+
+    public function getName()
+    {
+    	return 'porteAperteEstate';
+    }
+
+}
