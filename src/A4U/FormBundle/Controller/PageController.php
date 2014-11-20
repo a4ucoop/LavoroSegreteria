26149f622de6d1f0bb6e9dd18659876bc76f44f3
Michelangelo Diamanti <michelangelo.diamanti@gmail.com>
2014-11-20 11:49:33 +0100

    creata form generica vedere Timestampable

--- src/A4U/FormBundle/Controller/PageController.php
+++ src/A4U/FormBundle/Controller/PageController.php
@@ -8,9 +8,11 @@ use Symfony\Component\HttpFoundation\Request;
 
 use A4U\FormBundle\Entity\PorteAperteEstate;
 use A4U\FormBundle\Entity\Stage;
+use A4U\FormBundle\Entity\Generico;
 
 use A4U\FormBundle\Form\Type\PorteAperteEstateType;
 use A4U\FormBundle\Form\Type\StageType;
+use A4U\FormBundle\Form\Type\GenericoType;
 
 class PageController extends Controller
 {
@@ -107,6 +109,50 @@ class PageController extends Controller
     }
 
     public function showstageAction()
+    {
+        $Users = $this->getDoctrine()
+        ->getRepository('A4UFormBundle:Stage')
+        ->findAll();
+
+        if (!$Users)
+        {
+            throw $this->createNotFoundException('Nessun utente trovato');
+        }
+
+        return $this->render('A4UFormBundle:Forms:show_stage.html.twig', array(
+            'users' => $Users));
+    }
+
+// ---------------------------------------GENERICO--------------------------------------------
+
+    public function genericoAction(Request $request)
+    {
+        $Generico = new Generico();
+
+        $form = $this->createForm('generico', $Generico);
+
+        $form->handleRequest($request);
+
+        //Form inviato, viene validato
+        if ($form->isValid()) {
+
+            //Salva il nuovo utente nella base dati
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($Generico);
+            $em->flush();
+
+            return new Response('Creato l\'utente con id '.$Generico->getId(). ' in Generico');
+            //return $this->redirect($this->generateUrl('a4_u_form_success'));
+        }
+
+        // Form non ancora inviato
+        return $this->render('A4UFormBundle:Forms:generico.html.twig', array(
+            'form' => $form->createView(),
+        )); 
+
+    }
+
+    public function showgenericoAction()
     {
         $Users = $this->getDoctrine()
         ->getRepository('A4UFormBundle:Stage')
